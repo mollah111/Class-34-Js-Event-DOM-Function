46ee2f04f1c0f4ec3db5a6b26792def6cc15568a
mollah111 <jalammollah111@gmail.com>
2024-10-30 19:20:34 +0600

    DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function sum (){
+    let x = document.getElementById('number1').value;
+    let y = document.getElementById('number2').value;
+
+    let result = parseFloat(x) + parseFloat(y);
+
+    document.getElementById('result').innerHTML = "The result is =" + result;
+}
+
+
+
+
+
